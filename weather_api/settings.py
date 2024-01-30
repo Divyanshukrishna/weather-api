9ef3af490e0fb0f3283089f0e52c5543ebde7405
divyanshu <divyanshukrishna11@gmail.com>
2024-01-30 20:09:05 +0530

    new_collectstatic

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
-    BASE_DIR/"static"
+    BASE_DIR/"static",
 ]
-STATIC_ROOT = BASE_DIR/"staticfiles"
+
+STATIC_ROOT = BASE_DIR/'staticfiles' 
 
 MEDIA_URL='/media/'
-
 MEDIA_ROOT=os.path.join(BASE_DIR/"media")
 
 
